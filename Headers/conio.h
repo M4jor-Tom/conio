354f6d9f9da4fe57581804435797089187abfdd0
tom <tom-vautray@hotmail.fr>
2020-12-02 09:40:10 +0100

    init

---
+++ Headers/conio.h
@@ -0,0 +1,18 @@
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo);
+
+/* Restore old terminal i/o settings */
+void resetTermios(void);
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo);
+/* Read 1 character without echo */
+char getch(void);
+
+/* Read 1 character with echo */
+char getche(void);
